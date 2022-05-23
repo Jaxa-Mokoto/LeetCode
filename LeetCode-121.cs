78de8a696df68e03b1dbf82379e7d6afa150d898
Jaxa Mokoto <47665098+Jaxa-Mokoto@users.noreply.github.com>
2022-05-23 22:34:09 +0800

    Update LeetCode-121.cs

--- LeetCode-121.cs
+++ LeetCode-121.cs
@@ -1,5 +1,5 @@
 
-*/
+/*
 You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
@@ -26,7 +26,7 @@ Constraints:
 1 <= prices.length <= 105
 0 <= prices[i] <= 104
 
-/*
+*/
     
 using System;
 
@@ -50,8 +50,8 @@ public class StockBuying
             {
                 minBuyPrice = prices[i];
             }
-
             
+	    //find a good selling date
             else if (prices[i] - minBuyPrice > maxProfit)
             {
                 maxProfit = prices[i] - minBuyPrice ;
